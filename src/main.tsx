f6e27db14fc0706888b002958f47e33cdaf1a1df
Twene Twene Appiah <twenethomas36@gmail.com>
2025-07-16 04:36:16 +0100

    rolled back and added the analytics tot the components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { Analytics } from "@vercel/analytics/next"
-createRoot(document.getElementById("root")!).render(
-    <>
-        <Analytics />
-        <App />
-        <Analytics />
-    </>
-);
+import './index.css'
+
+createRoot(document.getElementById("root")!).render(<App />);
