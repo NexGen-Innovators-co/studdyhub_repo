efa058d62c01392de9bf9309ff5f9a5495cd8604
Twene Twene Appiah <twenethomas36@gmail.com>
2025-07-16 17:08:25 +0100

    change the ts for vercel

--- api/sitemap.xml.ts
+++ api/sitemap.xml.ts
@@ -1,6 +1,8 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
+/**
+ * @param {import('@vercel/node').VercelRequest} req
+ * @param {import('@vercel/node').VercelResponse} res
+ */
+export default function handler(req, res) {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
