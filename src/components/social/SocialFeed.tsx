8058a7412231553857561cc4818337d30ebfc3ff
Twene Twene Appiah <twenethomas36@gmail.com>
2025-08-25 06:00:59 +0100

    added the logout to sidebar

--- src/components/social/SocialFeed.tsx
+++ src/components/social/SocialFeed.tsx
@@ -94,7 +94,7 @@ export const SocialFeed: React.FC<SocialFeedProps> = ({ userProfile }) => {
   };
 
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-transparent bg-opacity-75 ">
       <div className="container mx-auto px-4 py-6">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
           {/* Main Content */}
